2a2b56455bc25b4706b5d12b8adfda6aaf3e233f
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-27 06:33:19 +0300

    completed assingment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MovieList";
+import MovieShow from "./MovieShow";
 
-function MoviesPage({ movies }) {
+const MoviesPage = ({ movies }) => {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${match.url}/:movieId`}>
+        {/* Pass the `movies` prop to MovieShow */}
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
-}
+};
+
 export default MoviesPage;
